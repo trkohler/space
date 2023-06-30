50258abe0312036153687cd0be05b1a8091e74b6
Troy Kohler <troy.kohler@zalando.de>
2023-06-30 12:54:58 +0200

    minor refactor

--- api/src/graphql/mutation/registration.rs
+++ api/src/graphql/mutation/registration.rs
@@ -1,13 +1,11 @@
 use crate::db::Database;
-use async_graphql::{Context, Object, Result, InputObject, Error as GraphQLError, ErrorExtensions};
-use axum_extra::extract::cookie::Cookie;
-use cookie::time::Duration;
+use async_graphql::{Context, InputObject, Object, Result};
 
-use crate::{AppState, SecretStore};
+use crate::SecretStore;
 use entity::sea_orm::DbErr;
 use entity::user::UserNode;
 use google_oauth::{AsyncClient, GooglePayload};
-use graphql_example_service::{Mutation, Query};
+use graphql_example_service::Mutation;
 use thiserror::Error;
 
 #[derive(Default)]
@@ -24,18 +22,16 @@ pub enum RegisterUserError {
     InvalidDataProvided { missing_field: String },
 
     #[error("google error: {0}")]
-    GoogleError(String),
+    GoogleError(#[from] anyhow::Error),
 
     #[error("Database error: {0}")]
     DbError(#[from] DbErr),
-}
 
-impl ErrorExtensions for RegisterUserError {
-    fn extend(&self) -> GraphQLError {
-        GraphQLError::new(format!("{:?}", self)).extend_with(|err, e| {
-            e.set("code", 500);
-        })
-    }
+    #[error("Failed to extract data from context")]
+    ExtractContextError,
+
+    #[error("No secret provided")]
+    SecretIsMissing,
 }
 
 #[Object]
@@ -45,61 +41,43 @@ impl RegistrationMutation {
         ctx: &Context<'_>,
         user_input: UserInput,
     ) -> Result<UserNode, RegisterUserError> {
-
         let db = ctx.data::<Database>().unwrap();
 
         let client_id = ctx
             .data::<SecretStore>()
-            .unwrap()
+            .map_err(|_| RegisterUserError::ExtractContextError)?
             .get("GOOGLE_CLIENT_ID")
-            .unwrap();
+            .ok_or(RegisterUserError::SecretIsMissing)?;
 
         let client = AsyncClient::new(client_id);
 
         let data = client
             .validate_id_token(user_input.google_jwt_token.clone())
-            .await;
-        match data {
-            Ok(data) => {
-                let GooglePayload {
-                    email,
-                    family_name,
-                    given_name,
-                    exp,
-                    ..
-                } = data;
+            .await
+            .map_err(|e| RegisterUserError::GoogleError(e))?;
 
-                let email = email.ok_or(RegisterUserError::InvalidDataProvided {
-                    missing_field: "email".to_owned(),
-                })?;
-                let family_name = family_name.ok_or(RegisterUserError::InvalidDataProvided {
-                    missing_field: "family_name".to_owned(),
-                })?;
-                let given_name = given_name.ok_or(RegisterUserError::InvalidDataProvided {
-                    missing_field: "given_name".to_owned(),
-                })?;
-                let display_name = given_name.clone() + " " + &family_name;
+        let GooglePayload {
+            email,
+            family_name,
+            given_name,
+            ..
+        } = data;
 
-                let expiring = Duration::from(Duration::seconds(exp as i64));
+        let email = email.ok_or(RegisterUserError::InvalidDataProvided {
+            missing_field: "email".to_owned(),
+        })?;
+        let family_name = family_name.ok_or(RegisterUserError::InvalidDataProvided {
+            missing_field: "family_name".to_owned(),
+        })?;
+        let given_name = given_name.ok_or(RegisterUserError::InvalidDataProvided {
+            missing_field: "given_name".to_owned(),
+        })?;
+        let display_name = format!("{} {}", given_name, family_name);
 
-                let node = Mutation::register_user(db.get_connection(), email, display_name)
-                    .await
-                    .map_err(|e| RegisterUserError::DbError(e))?;
+        let node = Mutation::register_user(db.get_connection(), email, display_name)
+            .await
+            .map_err(|e| RegisterUserError::DbError(e))?;
 
-                let cookie: Cookie =
-                    Cookie::build("google_token", user_input.google_jwt_token.clone())
-                        .path("/")
-                        .secure(true)
-                        .same_site(cookie::SameSite::None)
-                        .http_only(true)
-                        .max_age(expiring)
-                        .finish();
-
-                ctx.insert_http_header("Set-Cookie", cookie.to_string());
-
-                Ok(node)
-            }
-            Err(e) => Err(RegisterUserError::GoogleError(e.to_string())),
-        }
+        Ok(node)
     }
 }
